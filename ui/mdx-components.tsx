e71ec2a201fff38ebd081fc3941381fe3a1c1893
Acrylic125 <acrylic125email@gmail.com>
2026-04-11 12:29:56 +0800

    feat: help page

--- ui/mdx-components.tsx
+++ ui/mdx-components.tsx
@@ -2,20 +2,20 @@ import type { MDXComponents } from "mdx/types";
 
 const components: MDXComponents = {
   h1: ({ children }) => (
-    <h1 className="text-3xl lg:text-4xl pb-2 lg:pb-4 font-bold">{children}</h1>
+    <h1 className="text-3xl lg:text-4xl py-2 lg:py-4 font-bold">{children}</h1>
   ),
   h2: ({ children }) => (
-    <h2 className="text-xl lg:text-2xl pb-2 lg:pb-4 font-bold">{children}</h2>
+    <h2 className="text-xl lg:text-2xl py-2 lg:py-4 font-bold">{children}</h2>
   ),
   h3: ({ children }) => (
-    <h3 className="text-lg lg:text-xl pb-2 lg:pb-4 font-bold">{children}</h3>
+    <h3 className="text-base lg:text-lg py-2 lg:py-4 font-bold">{children}</h3>
   ),
-  h4: ({ children }) => (
-    <h4 className="text-base lg:text-lg pb-2 lg:pb-4 font-bold">{children}</h4>
-  ),
-  h5: ({ children }) => (
-    <h5 className="text-sm lg:text-base pb-2 lg:pb-4 font-bold">{children}</h5>
-  ),
+  //   h4: ({ children }) => (
+  //     <h4 className="text-base lg:text-lg pb-2 lg:pb-4 font-bold">{children}</h4>
+  //   ),
+  //   h5: ({ children }) => (
+  //     <h5 className="text-sm lg:text-base pb-2 lg:pb-4 font-bold">{children}</h5>
+  //   ),
   p: ({ children }) => (
     <p className="text-sm lg:text-base pb-2 lg:pb-4">{children}</p>
   ),
@@ -23,10 +23,26 @@ const components: MDXComponents = {
     <pre className="w-[calc(100vw-2rem)] overflow-auto">{children}</pre>
   ),
   a: ({ children, href }) => (
-    <a href={href} target="_blank" rel="noopener noreferrer">
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-primary-500 no-underline"
+    >
       {children}
     </a>
   ),
+  ul: ({ children }) => (
+    <ul className="flex flex-col gap-2 list-disc list-inside pb-2 lg:pb-4">
+      {children}
+    </ul>
+  ),
+  ol: ({ children }) => (
+    <ol className="flex flex-col gap-2 list-decimal list-inside pb-2 lg:pb-4">
+      {children}
+    </ol>
+  ),
+  li: ({ children }) => <li className="text-sm lg:text-base">{children}</li>,
 };
 
 export function useMDXComponents(): MDXComponents {
--- ui/src/app/help/layout.tsx
+++ ui/src/app/help/layout.tsx
@@ -5,7 +5,7 @@ export default function HelpLayout({
 }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="flex flex-col gap-12 py-4 lg:py-6 xl:py-8 max-w-ui w-full px-4">
+      <div className="flex flex-col gap-12 py-4 lg:py-6 xl:py-8 max-w-6xl w-full px-4">
         <div className="flex flex-col">{children}</div>
       </div>
     </div>
